25fab0c1ebc5c5aa5eae26648f4a3d982f27a452
Brad Garropy <bradgarropy@gmail.com>
2025-02-17 22:14:10 -0600

    done with part two.

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,14 @@
 import "dotenv/config"
 
+import os from "node:os"
+
 import {v2 as cloudinary} from "cloudinary"
 
-cloudinary.config({cloud_name: process.env.CLOUD_NAME})
+cloudinary.config({
+    cloud_name: process.env.CLOUD_NAME,
+    api_key: process.env.API_KEY,
+    api_secret: process.env.API_SECRET,
+})
 
 // PART ONE - Adding existing subtitles
 
@@ -10,10 +16,32 @@ const video = cloudinary.url("cloudinary-node-subtitle-demo/test", {
     resource_type: "video",
     overlay: {
         resource_type: "subtitles",
-        public_id: "cloudinary-node-subtitle-demo/test.srt",
+        public_id: "cloudinary-node-subtitle-demo/test.transcript",
     },
 })
 
 console.log(video)
 
 // PART TWO - Adding generated subtitles
+
+const path = `${os.homedir()}/Desktop/cloudinary-node-subtitle-demo/generated.mp4`
+
+await cloudinary.uploader.upload(path, {
+    public_id: "generated",
+    folder: "cloudinary-node-subtitle-demo",
+    resource_type: "video",
+    raw_convert: "google_speech:srt:vtt",
+})
+
+const uploadedVideo = cloudinary.url(
+    "cloudinary-node-subtitle-demo/generated",
+    {
+        resource_type: "video",
+        overlay: {
+            resource_type: "subtitles",
+            public_id: "cloudinary-node-subtitle-demo/generated.transcript",
+        },
+    },
+)
+
+console.log(uploadedVideo)
