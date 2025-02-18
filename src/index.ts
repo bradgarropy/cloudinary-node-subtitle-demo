d3406bb2e8bbd706e0cf21730dca1115ee4784e6
Brad Garropy <bradgarropy@gmail.com>
2025-02-17 21:19:28 -0600

    progress on part one.

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,19 @@
-import {sayHello} from "./hello"
+import "dotenv/config"
 
-sayHello()
+import {v2 as cloudinary} from "cloudinary"
+
+cloudinary.config({cloud_name: process.env.CLOUD_NAME})
+
+// PART ONE - Adding existing subtitles
+
+const video = cloudinary.url("cloudinary-node-subtitle-demo/test", {
+    resource_type: "video",
+    overlay: {
+        resource_type: "subtitles",
+        public_id: "cloudinary-node-subtitle-demo/test.srt",
+    },
+})
+
+console.log(video)
+
+// PART TWO - Adding generated subtitles
